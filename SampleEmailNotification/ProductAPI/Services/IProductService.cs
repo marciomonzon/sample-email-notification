5262af4ff5623f71713d7e16d31832a729059f1c
marciomonzon <marcio.pcmonzon@gmail.com>
2025-01-20 20:33:41 -0300

    add product service and its interface

---
+++ SampleEmailNotification/ProductAPI/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ProductAPI.DTO;
+
+namespace ProductAPI.Services
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAllAsync();
+        Task<ProductDto> GetByIdAsync(int id);
+        Task AddAsync(ProductDto productDto);
+        Task UpdateAsync(int id, ProductDto productDto);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ SampleEmailNotification/ProductAPI/Services/ProductService.cs
@@ -0,0 +1,65 @@
+﻿using ProductAPI.Data.Repository;
+using ProductAPI.DTO;
+using ProductAPI.Models;
+using ProductAPI.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<List<ProductDto>> GetAllAsync()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return products.Select(p => new ProductDto(p.Id,
+                                                   p.Name,
+                                                   p.Description,
+                                                   p.Price,
+                                                   p.Stock))
+                                                   .ToList();
+    }
+
+    public async Task<ProductDto> GetByIdAsync(int id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        if (product == null) return null;
+
+        return new ProductDto(product.Id, product.Name, product.Description, product.Price, product.Stock);
+    }
+
+    public async Task AddAsync(ProductDto productDto)
+    {
+        var product = new Product
+        {
+            Id = productDto.Id,
+            Name = productDto.Name,
+            Description = productDto.Description,
+            Price = productDto.Price,
+            Stock = productDto.Stock
+        };
+
+        await _productRepository.AddAsync(product);
+    }
+
+    public async Task UpdateAsync(int id, ProductDto productDto)
+    {
+        var existingProduct = await _productRepository.GetByIdAsync(id);
+        if (existingProduct == null) return;
+
+        existingProduct.Name = productDto.Name;
+        existingProduct.Description = productDto.Description;
+        existingProduct.Price = productDto.Price;
+        existingProduct.Stock = productDto.Stock;
+
+        await _productRepository.UpdateAsync(existingProduct);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _productRepository.DeleteAsync(id);
+    }
+}
